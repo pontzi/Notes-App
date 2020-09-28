425e62be9594594eb2d5c5b01b1ebaafc3cda36b
Ismael Ponce <ImDevPonce@outlook.com>
2020-09-28 18:05:51 -0500

    modified footer

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ require("./database");
 require("./config/passport");
 
 //Settings
-app.set("port", process.env.PORT || 9000);
+app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.engine(
   ".hbs",
